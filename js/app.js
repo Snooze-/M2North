f63720301f96c8b61431cc5e69930b2b91f53e14
Derick Kemp <derick@derick.co.za>
2015-07-16 17:40:23 +0200

    Weather data retrieved
    The front end (index.html) now successfully loads weather data when a
    city is selected from the input.
    
    Currently results are only logged to the console.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 $( document ).ready(function() {
+    // This function makes the AJAX call to our PHP page to get weather info and
+    // then displays the results on the page.
+    showForecast = function(item) {
+        $.get('/forecast.php', { 'zip': item.zip }, function(data) {
+            console.log(data);
+        }, 'json');
+    };
+
     // We're using the bootstrap3-typehead library to select the correct town
-    $.get('/states.json', function(data){
+    $.get('/states.json', function(data) {
         $(".typeahead").typeahead({
             source: data,
             displayText: function(item) {
                 return item.name + ', ' + item.state;
-            }
+            },
+            afterSelect: showForecast
         });
-    },'json');
+    }, 'json');
 });

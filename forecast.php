b135324d70d0fb723057e166539fe20a7bb93385
Derick Kemp <derick@derick.co.za>
2015-07-17 04:26:42 +0200

    Fixing bug
    In forecast.php a string is passed as a litteral.

--- forecast.php
+++ forecast.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(implode(DIRECTORY_SEPARATOR, array(__DIR__, krumo, 'class.krumo.php')));
+require_once(implode(DIRECTORY_SEPARATOR, array(__DIR__, 'krumo', 'class.krumo.php')));
 
 $client = new SoapClient("http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL");
 

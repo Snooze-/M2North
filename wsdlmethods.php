3da566791c844408c42fef669545beaec50dd3d7
Derick Kemp <derick@derick.co.za>
2015-07-17 03:40:52 +0200

    Removed the wsdlmethods.php file
    wsdlmethods.php was an interesting little file that showed all the
    methods the wsdl exposes, but didn't add much value.

--- wsdlmethods.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once(implode(DIRECTORY_SEPARATOR, array(__DIR__, krumo, 'class.krumo.php')));
-
-$client = new SoapClient("http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL");
-
-krumo($client->__getFunctions());
